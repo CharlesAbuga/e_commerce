3196dcc467433e983f533391e681b5b2b8ccc9ea
unknown <godfreycharles30@gmail.com>
2024-02-18 16:20:48 +0300

    first commit

---
+++ lib/widgets/product_card.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class ProductCard extends StatelessWidget {
+  const ProductCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 210,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        color: Colors.white,
+        surfaceTintColor: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: Image.asset(
+                    fit: BoxFit.cover,
+                    width: 210,
+                    height: 160,
+                    'assets/images/jeans.jpeg'),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 8.0, right: 8.0),
+                child: Text(
+                  'Jeans',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 14,
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 8, right: 8.0),
+                child: ListTile(
+                  contentPadding: EdgeInsets.zero,
+                  leadingAndTrailingTextStyle: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                  leading: Text('Ksh.500'),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 8.0),
+                child: ListTile(
+                  contentPadding: EdgeInsets.zero,
+                  leading: IconButton(
+                    style: IconButton.styleFrom(
+                      backgroundColor: Colors.black,
+                    ),
+                    color: Colors.white,
+                    hoverColor: Colors.orange,
+                    onPressed: () {},
+                    icon: const Icon(Icons.shopping_cart),
+                  ),
+                  trailing: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              Theme.of(context).colorScheme.primary),
+                      child: Text('Buy Now',
+                          style: TextStyle(
+                              color: Theme.of(context).colorScheme.secondary))),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
